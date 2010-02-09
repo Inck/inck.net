42e88eb3b00de85fd949a7dbeed8e60f2e25769d
Nicholas Hall <nick@inck.net>
2010-02-09 03:31:04 +0000

    Added article. Deleted js folder temporarily to repair .svn meta data. Set fixed six_column photograph height.

--- index.php
+++ index.php
@@ -44,13 +44,13 @@
 					<li class="set three_columns contained">
 						<ul>
 							<li id="top_right_one" class="module leader hyphenate">
-								<h1><a href="page.php?number=n1" name="n1">Real Things Happen</a></h1>
-								<cite>by Nicholas Hall on January 27th, 2010</cite>
-								<p><span class="dateline">Mérida</span> Real things happened yesterday and Monday in Mérida, as students took to the streets to protest the shutdown of RCTV, one the last remaining broadcast networks in Venezuela which had been openly critical of the Chávez government.</p>
-								<p>On Monday students were seen throwing rocks and moving in groups, armed with molotov cocktails fashioned out of regional beer bottles. Many had t-shirts tied around their faces. They blocked traffic in a series of streets in the downtown area, using chairs and tires that were then set on fire. </p>
-								<p>Police in riot gear massed throughout the downtown area over the course of the afternoon.</p>
-								<p>Later in the evening the downtown area had become impassable, as much due to a domino traffic effect as to specific blockades. Busses were not running into the the downtown area from the outlying neighborhoods, and taxis were largely unable to pass the lower streets.</p>
-								<p>Most people walked home, and the streets were relatively safe in all but the southern</p>
+								<h1><a href="page.php?number=c5" name="c5">Cultural Experience Had</a></h1>
+								<cite>by Nicholas Hall on February 8th, 2010</cite>
+								<p><span class="dateline">Mérida</span> Nick had a cultural experience Saturday, according to the hangover he had yesterday morning and the generalized sense of joy.</p>
+								<p>An event whose name translates roughly into "The Crowning of the Queen of the Fairs" was the specific event seen to have catalyzed this experience, but the experience was more broad and primarily tangential to the event.</p>
+								<p>The Crowning was held in the Plaza de Toros, normally used for bullfights, but on this the first night of the Fairs, used for a pop-music concert and beauty pageant, the winner of which was Crowned said Queen of said Fairs.</p>
+								<p>Salsa music, meringue music, and forms of popular music largely understood as issuing from salsa and meringue music, were played.</p>
+								<p>Additionally were seen the drinking of beer and plastic bottles of sangria, the throwing of the plastic bottles, fights, and an impromptu breakdancing session atop a dangerous internal roof.</p>
 							</li>
 						</ul>
 					</li>
@@ -62,17 +62,18 @@
 							<li class="set three_columns contained">
 								<ul>
 									<li id="top_right_two" class="module leader hyphenate">
-										<p>quadrant of the city's center.</p>
-										<p>In the evening, reports circulated in the city about a young man who had died of a gunshot wound during clashes between pro- and anti-Chavez groups, after this event, and due to escalating conflict later in the night, the city center was seen as impassable.</p>
-										<p>In the morning it was reported that another young man had been shot and killed during the night.</p>
+										<p>The breakdancing culminated in a young man somersaulting off of the dangerous roof and landing on the ground where he was then immobile.</p>
+										<p>Rings of people then surrounded him, paramedics at the center, police around them, and concerned citizens and rubberneckers around them. </p>
+										<p>Chants of 'idiota' briefly replaced those of other things to do with musicians and beauty</p>
 									</li>
 								</ul>
 							</li>
 							<li class="set three_columns">
 								<ul>
 									<li id="top_right_three" class="module leader hyphenate">
-										<p>Protests continued throughout Thursday, and all classes at the school attended by this reporter were canceled. Shouts and chanting could be heard in the streets from the downtown bureau, which is this reporter's homestay for the next two weeks.</p>
-										<p>Today life had largely returned to normal, and irrelevant topics were once again under consideration for further coverage.</p>
+										<p>pageant contestants as the young man lay motionless.</p>
+										<p>The young man was then carried on a stretcher towards an exit, through the center of the plaza.</p>
+										<p>Reaching the center of the plaza the young man sprang up and careened dizzily towards the crowd with his arms raised to signal his victory over head trauma. <a href="page.php?number=C5&from=283#c5">Continue on Page C5 &raquo;</a></p>
 									</li>
 								</ul>
 							</li>
@@ -142,12 +143,12 @@
 							<li class="set three_columns">
 								<ul>
 									<li class="module leader hyphenate">
-										<h2><a href="page.php?number=t4" name="t4">Venezuelan Nerds Found</a></h2>
-										<cite>by Nicholas Hall on January 23rd, 2010</cite>
-										<p><span class="dateline">Mérida</span> Venezuelan nerds were found today, in La Facultad de Ingeniería, a building of the Universidad de Los Andes in La Hechizera, a neighborhood set into the mountains above Mérida.</p>
-										<p>Wearing glasses, slouching, and installing Ubuntu, these nerds were found to be nerds very similar to those common in the United States.</p>
-										<p>They were also found to be earnest, smart, and avidly practicing their yo-yo skills.</p>
-										<p>Websites were shared and programing languages discussed. There was agreement in the excellence of CodeIgniter, and in the eventual interestingness of learning Ruby on Rails. <a href="page.php?number=t4&from=91#t4">Continue on Page T4 &raquo;</a></p>
+										<h2><a href="page.php?number=n1" name="n1">Real Things Happen</a></h2>
+										<cite>by Nicholas Hall on January 27th, 2010</cite>
+										<p><span class="dateline">Mérida</span> Real things happened yesterday and Monday in Mérida, as students took to the streets to protest the shutdown of RCTV, one the last remaining broadcast networks in Venezuela which had been openly critical of the Chávez government.</p>
+										<p>On Monday students were seen throwing rocks and moving in groups, armed with molotov cocktails fashioned out of regional beer bottles. Many had t-shirts tied around their faces. They blocked traffic in a series of streets in the downtown area, using chairs and tires that were then set on fire. </p>
+										<p>Police in riot gear massed throughout the downtown area over the course of the afternoon.</p>
+										<p>Later in the <a href="page.php?number=n1&from=106#n1">Continue on Page N1 &raquo;</a></p>
 									</li>
 								</ul>
 							</li>
