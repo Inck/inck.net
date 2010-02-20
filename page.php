12a35b18577cb4864b6977b7c97127714d4d77c3
Nicholas Hall <nick@inck.net>
2010-02-20 17:49:39 +0000

    Corrected headline in new article file. Added sorting and descending order to comments.

--- page.php
+++ page.php
@@ -152,6 +152,11 @@
 <?php
 		$letters_together = file_get_contents("letters/$number.txt");
 		$letters = explode("\n\n-----------------\n\n", $letters_together); array_pop($letters);
+
+		function letters_date_sort($a, $b) { return $a[1] - $b[1]; }
+		uasort($letters, 'letters_date_sort');
+		$letters = array_reverse($letters, true);
+
 		$i = 0;
 		foreach($letters as $letter) {
 			$lines = explode("\n", $letter);
