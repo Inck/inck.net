d7b567e17c1dfca6075568c01b5e34714a042375
Nicholas Hall <nick@inck.net>
2010-02-23 14:29:06 +0000

    Added new article.

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 				<ul>
 					<li class="module header_divider"></li>
 					<li class="module assassination">
-						<h1><a href="page.php?number=c5" name="c5">Cultural Experience Had</a></h1>
+						<h1><a href="page.php?number=c7" name="c7">Bullfight Attended, Analyzed</a></h1>
 					</li>
 				</ul>
 			</li>
@@ -47,36 +47,49 @@
 					<li class="set three_columns contained">
 						<ul>
 							<li id="top_right_one" class="module leader hyphenate">
-								<h2><a href="page.php?number=c5" name="c5">Cultural Experience Had</a></h2>
-								<cite>by Nicholas Hall on <em>February 8th, 2010</em></cite>
-								<p><span class="dateline">Mérida</span> Nick had a cultural experience Saturday, according to the hangover he had Sunday morning and the generalized sense of joy.</p>
-								<p>An event whose name translates roughly into "The Crowning of the Queen of the Fairs" was the specific event seen to have catalyzed this experience, but the experience was more broad and primarily tangential to the event.</p>
-								<p>The Crowning was held in the Plaza de Toros, normally used for bullfights, but on this the first night of the Fairs, used for a pop-music concert and beauty pageant, the winner of which was Crowned said Queen of said Fairs.</p>
-								<p>Salsa music, meringue music, and forms of popular music largely understood as issuing from salsa and meringue music, were played.</p>
-								<p>Additionally were seen the drinking of beer and plastic bottles of sangria, the throwing of the plastic bottles, fights, and an impromptu breakdancing session atop a dangerous internal roof.</p>
+								<h2><a href="page.php?number=c7" name="c7">Bullfight Attended, Analyzed</a></h2>
+								<cite>by Nicholas Hall on <em>February 23rd, 2010</em></cite>
+								<p><span class="dateline">Mérida</span> A bullfight was attended Saturday, and analyzed for specific cultural, aesthetic and moral features, according to a new report issued by Nick.</p>
+								<p>Many interesting judgements and criticisms were drawn from these observations.</p>
+								<p>The morality of the bullfight was found to be the least interesting point of analysis by our expert.</p>
+								<p>Morality was determined to be essentially counter-analytical. Further, moral arguments against the killing of animals in an emotionally rousing form rather than an industrial one were found to be specious. </p>
+								<p>Perhaps most alarmingly, assertions of the animals' suffering showed strong correlations with anthropomorphism and with a silly emotional reaction disguised as an ethical objection.</p>
+								<p>Culturally many things were taken to be intimated by the event, including things concerning power structures, gender roles, a fundamental humanism, drinking habits, death, and colonialism.</p>
 							</li>
 						</ul>
 					</li>
 					<li class="set six_columns">
 						<ul>
 							<li class="module photograph">
-								<a href="http://www.flickr.com/photos/nicholashall/4377103220/" title="Untitled by Nicholas Hall, on Flickr"><img src="http://farm5.static.flickr.com/4059/4377103220_56d6b803a5_b.jpg" alt="" /></a>
+								<a href="http://www.flickr.com/photos/nicholashall/4377208562/" title="Untitled by Nicholas Hall, on Flickr"><img src="http://farm3.static.flickr.com/2704/4377208562_ca91a6f991_b.jpg" alt="" /></a>
 							</li>
 							<li class="set three_columns contained">
 								<ul>
 									<li id="top_right_two" class="module leader hyphenate">
-										<p>The breakdancing culminated in a young man somersaulting off of the dangerous roof and landing on the ground where he was then immobile.</p>
-										<p>Rings of people then surrounded him, paramedics at the center, police around them, and concerned citizens and rubberneckers around them. </p>
-										<p>Chants of 'idiota' briefly replaced those of other things to do with musicians and beauty</p>
+										<p>One or two of these things were remembered from without the haze of sangria the next day; the rest presumed either forgotten or never worth remembering.</p>
+										<p>Those ideas that were not forgotten remain insufficiently conclusive for publication. They will be investigated further while drunk for a later report, according to the researcher, who was drunk at press time.</p>
+										<p>Most strikingly implied by the report were aesthetic findings.</p>
+										<p>It was concluded from available evidence that</p>
 									</li>
 								</ul>
 							</li>
 							<li class="set three_columns">
 								<ul>
 									<li id="top_right_three" class="module leader hyphenate">
-										<p>pageant contestants as the young man lay motionless.</p>
-										<p>The young man was then carried on a stretcher towards an exit, through the center of the plaza.</p>
-										<p>Reaching the center of the plaza the young man sprang up and careened dizzily towards the crowd with his arms raised to signal his victory over head trauma. <a href="page.php?number=C5&from=283#c5">Continued, with Letters to the Editor, on Page C5 &raquo;</a></p>
+										<p>the bullfight was very beautiful. The confusion of the animal, the glittering surface of the man, the mad profusion of the crowd, these were seen to report upon a topic of grave relevance and reality.</p>
+										<p>Seen was an iconography of man's stature between God and nature, the trajectory of every man from pliant creation to a hard death, all the while lashing downward, ultimately lashed by whatever is above him.</p>
+										<?php
+											$number_of_letters = count(explode("\n\n-----------------\n\n", file_get_contents("letters/c7.txt"))) - 1;
+											if($number_of_letters) {
+										?>
+																<p class="letters">(There <?php echo ($number_of_letters - 1) ? "are" : "is"; ?> currently <?php echo $words[$number_of_letters]; ?> Letter<?php echo $number_of_letters - 1 ? "s" : ""; ?> to the Editor in response to this article.) <a href="page.php?number=c7#top">Read them</a> and <a href="page.php?number=c7&write=true#top">write one</a>.</p>
+										<?php
+											} else {
+										?>
+																<p class="letters">(There are currently no Letters to the Editor in response to this article.) <a href="page.php?number=c7&write=true#top">Write one</a>.</p>
+										<?php
+											}
+										?>
 									</li>
 								</ul>
 							</li>
