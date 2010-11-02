e887c6ae0b7365b37ae7fefcd8e834af449b411d
Nicholas Hall <nick@inck.net>
2010-11-02 19:56:02 +0000

    Removed extraneous hyphenation JS library.

--- inc/foot.php
+++ inc/foot.php
@@ -9,6 +9,5 @@
 		</ul>
 		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
 		<script type="text/javascript"> _uacct = "UA-2336235-2"; urchinTracker(); </script>
-		<script type="text/javascript" src="js/lib/sweet-justice.min.js"></script>
 	</body>
 </html>
