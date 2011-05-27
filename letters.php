548c995419cd29dc17a4a5607f78a0ccc052dd21
Nicholas Hall <nick@inck.net>
2011-05-27 03:20:38 +0000

    Un-ignored letters for eventual committing by production for archiving. Fixed internal jump in article.
    Modified letters page to show only current a1 articles, and to offer a link if none are present.

--- letters.php
+++ letters.php
@@ -1,7 +1,15 @@
 <?php
-	// ToDo: Limit to a1 articles.
 	$title = "Letters to the Editor";
 	include('inc/head.php');
+	
+	// ToDo: Limit to a1 articles.
+	// Get article list from feed.
+	$feed_xml = `php feed.php`;
+	$feed = new SimpleXMLElement($feed_xml);
+	$article_numbers[] = substr($feed->channel->item[0]->guid, 32);
+	$article_numbers[] = substr($feed->channel->item[1]->guid, 32);
+	$article_numbers[] = substr($feed->channel->item[2]->guid, 32);
+	$article_numbers[] = substr($feed->channel->item[3]->guid, 32);
 ?>
 			<li class="column one_unit">
 				<ul>
@@ -11,7 +19,7 @@
 				</ul>
 			</li>
 			<li class="space nine_units"></li>
-			<li class="column two_units rule_at_left">
+			<li class="column two_units">
 				<ul>
 					<li class="block edition secondary">
 						<cite><?php echo $edition; ?></cite>
@@ -25,37 +33,26 @@
 				</ul>
 			</li>
 <?php
-	$directory = opendir('letters');
-	while($filename = readdir($directory)) {
-		if($filename[0] != ".") {
-			$filenames[] = $filename;
-		}
-	}
-	
-	$filenames = array_reverse(array_slice($filenames, -3));
-	
-	foreach($filenames as $filename) {
+	foreach($article_numbers as $key => $article_number) {
 ?>
-			<li class="column three_units">
+			<li class="column three_units<?php if(!$key) echo ' contained'; ?>">
 				<ul>
 <?php
-		$article = file_get_contents('pages/' . $filename); $lines = explode("\n", $article);
+		$article = file_get_contents('pages/' . $article_number . '.txt'); $lines = explode("\n", $article);
 		$article_title = trim($lines[0]);
 		$article_date = trim($lines[1]);
-		$article_number = substr($filename, 0, -4);
-		$prompt_paragraph = "Having read '$article_title', I have the following comment.";
-		$letters_together = file_get_contents('letters/' . $filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-		$letters = explode("\n\n-----------------\n\n", $letters_together); array_pop($letters);
 ?>
 					<li class="block leader contained">
 						<h2><a href="page.php?number=<?php echo $article_number; ?>"><?php echo $article_title; ?></a></h2>
 						<cite>by Nicholas Hall on <em><?php echo $article_date; ?></em></cite>
 					</li>
 <?php
-		foreach($letters as $letter) {
-			$lines = explode("\n", $letter);
-			$time = array_shift($lines);
-			$name = array_pop($lines);
+		if($letters_together = file_get_contents('letters/' . $article_number . '.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) {
+			$letters = explode("\n\n-----------------\n\n", $letters_together); array_pop($letters);
+			foreach($letters as $letter) {
+				$lines = explode("\n", $letter);
+				$time = array_shift($lines);
+				$name = array_pop($lines);
 ?>
 					<li class="block letter_box two_units">
 						<ul>
@@ -63,14 +60,14 @@
 								<em><?php if(date('zY') != date('zY', $time)) { echo date('F jS, Y', $time); } else { echo date('g:i a', $time); } ?></em>
 								<p class="read">Dear Sir:</p>
 <?php
-			foreach($lines as $line) {
-				$line = trim($line);
-				if($line) {
+				foreach($lines as $line) {
+					$line = trim($line);
+					if($line) {
 ?>
 								<p><?php echo $line; ?></p>
 <?php
+					}
 				}
-			}
 ?>
 								<p class="read">Sincerely,</p>
 								<cite><a href="" rel="nofollow"><?php echo $name; ?></a></cite>
@@ -78,11 +75,17 @@
 						</ul>
 					</li>
 <?php
+			}
+		} else {
+?>
+					<li class="block leader contained">
+						<p><a href="page.php?number=<?php echo $article_number; ?>">Write a Letter to the Editor.</a></p>
+					</li>
+<?php
 		}
 ?>
 				</ul>
 			</li>
-			<li class="space one_unit"></li>
 <?php
 	}
 	include('inc/foot.php');
