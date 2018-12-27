7e1a834102af1e24177d1c0a5d712615d337e974
Nicholas Hall <477925+Inck@users.noreply.github.com>
2018-12-27 12:34:43 -0500

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -1,6 +1,6 @@
 <?php
 	date_default_timezone_set('GMT');
-	$edition = "Dark Financial Times Edition";
+	$edition = "When the Internet Was Fine Edition";
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
